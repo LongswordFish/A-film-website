280c94bd8608967a657b9bfde9b27236aaccf0df
longswordfish <robinyu9840@gmail.com>
2021-03-06 16:24:30 -0500

    The original a2

---
+++ public/js/movieListing.js
@@ -0,0 +1,31 @@
+// const listing=document.querySelector('#listing');
+
+// function clear(){
+//     listing.innerHTML='';
+// }
+
+// function clearSection() {
+//     var sec = document.querySelector('#listing');
+//     var secList = sec.querySelectorAll('.card');
+//     alert(secList.length);
+//     for (var i = 0; i < secList.length; i++) sec.removeChild(secList[i]);
+//   }
+
+// const movieBtn=document.querySelector('#movieBtn');
+
+// movieBtn.addEventListener('click',()=>{
+//     clearSection();
+//     alert('eee');
+// });
+
+
+// const TVBtn=document.querySelector('#TVBtn');
+// TVBtn.addEventListener('onclick',()=>{
+//     alert('eee');
+    
+    
+// });
+
+// listing.innerHTML="{{#each all}} <div class='card pic col-6 col-md-2 col-sm-3'> <p class='card-text' style='font-size: smaller; display:flex;justify-content: space-between;'> <span>Rent:${{this.price_to_rent}}</span> <span>Buy\:\${{this\.price_to_purchase}}</span\>
+// </p> <a href='/movies/{{this.id}}'><img src='{{this.small_picture}}' class='card-img' alt='...></a><div class='card-body'><h5 class='card-title'>{{\this.title}}</h5></div></div>{{/each}}";   
+// }
---
+++ server.js
@@ -0,0 +1,90 @@
+//     Student name: Jianchang (Robin) YU
+//     student number: 160210191
+//     student email: jyu205@myseneca.ca
+//     assignment: assignment 2 for web322
+//     date: 14 Feb. 2021
+//     version 2: 3 March 2021
+
+const express=require("express");
+const exphbs=require("express-handlebars");
+const bodyParser = require('body-parser');
+
+//import local module fakeDb 
+const movies = require("./model/FakeDB");
+
+const app = express();
+
+//which static routes are
+app.use(express.static("public"));
+
+//which template engine is used in this project
+app.engine('handlebars',exphbs());
+app.set('view engine', 'handlebars');
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+
+//routes
+//route for home page
+app.get("/",(req,res)=>{
+    console.log("home route")
+    res.render("home",{
+        title : "home",
+        featuredMovies: movies.getFeaturedMovies(),
+        featuredTVs: movies.getFeaturedTVs(),
+        crimes: movies.getCrimes()
+    });
+})
+
+
+//route for movieDetail
+app.get("/movies/:id",(req,res)=>{
+    res.render("movieDetail",{
+        title:"movieDetail",
+        movie: movies.getMovie(req.params.id)
+    });
+})
+
+//route for movieListing
+app.get("/listing/:type",(req,res)=>{
+
+    const type= req.params.type;
+    let films = [];
+
+    if(type=='movies')
+    {
+        films = movies.getMovies()
+    }
+
+    else if (type=="TVs")
+    {
+        films = movies.getTVs() 
+    }
+    else if (type=="all")
+    {
+        films = movies.getAllMovies()
+    }
+
+    res.render("movieListing",{
+        title:"movieListing",
+        movies: films
+    });
+})
+
+
+
+//404 error page
+app.use((req, res) => {
+    const path=__dirname+"/views/404.html";
+    res.sendFile(path);
+});
+
+//define the port used for this application
+const PORT = 3000;
+app.listen(PORT,()=>{
+    console.log(`Web page is listenning on port: ${PORT}`);
+})
+
+
+
